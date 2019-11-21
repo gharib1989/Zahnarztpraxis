cf4b39f886e14b932565f544de1f7f2a0811a238
gharib1989 <31074812+gharib1989@users.noreply.github.com>
2019-11-21 13:20:45 +0100

    fix comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'zhanArztPraxix';
+export class AppComponent  implements  OnInit , AfterViewInit {
+  @ViewChild( 'stickyMenu', {static: false}) public menuElement: ElementRef;
+  public title: string = 'zhanArztPraxix';
+  public sticky: boolean = false;
+  public menuPosition: any;
+  constructor() { }
+  public ngAfterViewInit() : void  {
+    this.menuPosition = this.menuElement.nativeElement.offsetTop;
+}
+@HostListener('window:scroll', ['$event'])
+public handleScroll() : void {
+        const windowScroll = window.pageYOffset;
+       console.log(windowScroll);
+        if ( windowScroll >= this.menuPosition) {
+           this.sticky = true;
+       } else {
+           this.sticky = false;
+       }
+    }
+    public ngOnInit(): void {
+    }
 }
